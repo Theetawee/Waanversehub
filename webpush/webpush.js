42bed3dfc27f81c0497082f435be8e09edec1960
Forever Inc dev <redodevs@gmail.com>
2023-10-30 14:37:55 +0300

    save

--- webpush/webpush.js
+++ webpush/webpush.js
@@ -1,141 +1,135 @@
-
 var isPushEnabled = false,
-    registration,
-    subBtn;
+  registration,
+  subBtn;
 
-window.addEventListener('load', function() {
-  subBtn = document.getElementById('webpush-subscribe-button');
+window.addEventListener("load", function () {
+  subBtn = document.getElementById("webpush-subscribe-button");
 
-  subBtn.addEventListener('click',
-    function() {
-      subBtn.disabled = true;
-      if (isPushEnabled) {
-        return unsubscribe(registration);
-      }
-      return subscribe(registration);
+  subBtn.addEventListener("click", function () {
+    subBtn.disabled = true;
+    if (isPushEnabled) {
+      return unsubscribe(registration);
     }
-  );
+    return subscribe(registration);
+  });
 
   // Do everything if the Browser Supports Service Worker
-  if ('serviceWorker' in navigator) {
-    const serviceWorker = document.querySelector('meta[name="service-worker-js"]').content;
-    navigator.serviceWorker.register(serviceWorker).then(
-      function(reg) {
-        registration = reg;
-        initialiseState(reg);
-      });
+  if ("serviceWorker" in navigator) {
+    const serviceWorker = document.querySelector(
+      'meta[name="service-worker-js"]'
+    ).content;
+    navigator.serviceWorker.register(serviceWorker).then(function (reg) {
+      registration = reg;
+      initialiseState(reg);
+    });
   }
   // If service worker not supported, show warning to the message box
   else {
-    showMessage(gettext('Device does not support Notifications'));
+    showMessage(gettext("Device does not support Notifications"));
   }
 
   // Once the service worker is registered set the initial state
   function initialiseState(reg) {
     // Are Notifications supported in the service worker?
-    if (!(reg.showNotification)) {
-        // Show a message and activate the button
-        subBtn.textContent = 'Allow Notifications';
-        showMessage(gettext('Notifications from Waanverse are not allowed.'));
-        return;
+    if (!reg.showNotification) {
+      // Show a message and activate the button
+      subBtn.textContent = "Allow Notifications";
+      showMessage(gettext("Device not supported"));
+      return;
     }
 
     // Check the current Notification permission.
     // If its denied, it's a permanent block until the
     // user changes the permission
-    if (Notification.permission === 'denied') {
+    if (Notification.permission === "denied") {
       // Show a message and activate the button
-      subBtn.textContent = gettext('Allow Notifications');
+      subBtn.textContent = gettext("Allow Notifications");
       subBtn.disabled = false;
-      showMessage(gettext('Grant Waanverse permission to send you Notifications'));
+      showMessage(gettext(""));
       return;
     }
 
     // Check if push messaging is supported
-    if (!('PushManager' in window)) {
+    if (!("PushManager" in window)) {
       // Show a message and activate the button
-      subBtn.textContent = 'Allow Notifications';
+      subBtn.textContent = "Allow Notifications";
       subBtn.disabled = false;
-      showMessage(gettext('Device not supported'));
+      showMessage(gettext("Device not supported"));
       return;
     }
 
     // We need to get subscription state for push notifications and send the information to server
-    reg.pushManager.getSubscription().then(
-      function(subscription) {
-        if (subscription){
-          postSubscribeObj('subscribe', subscription,
-            function(response) {
-              // Check the information is saved successfully into server
-              if (response.status === 201) {
-                // Show unsubscribe button instead
-                subBtn.textContent = gettext('Restrict notifications from Waanverse');
-                subBtn.disabled = false;
-                isPushEnabled = true;
-                showMessage(gettext('Permission granted successfully'));
-              }
-            });
-        }
-      });
+    reg.pushManager.getSubscription().then(function (subscription) {
+      if (subscription) {
+        postSubscribeObj("subscribe", subscription, function (response) {
+          // Check the information is saved successfully into server
+          if (response.status === 201) {
+            // Show unsubscribe button instead
+            subBtn.textContent = gettext("");
+            subBtn.disabled = true;
+            isPushEnabled = true;
+            showMessage(gettext(""));
+          }
+        });
+      }
+    });
   }
-}
-);
+});
 
 function showMessage(message) {
-  const messageBox = document.getElementById('webpush-message');
+  const messageBox = document.getElementById("webpush-message");
   if (messageBox) {
     messageBox.textContent = message;
-    messageBox.style.display = 'block';
+    messageBox.style.display = "block";
   }
 }
 
 function subscribe(reg) {
   // Get the Subscription or register one
-  reg.pushManager.getSubscription().then(
-    function(subscription) {
-      var metaObj, applicationServerKey, options;
-      // Check if Subscription is available
-      if (subscription) {
-        return subscription;
-      }
-
-      metaObj = document.querySelector('meta[name="django-webpush-vapid-key"]');
-      applicationServerKey = metaObj.content;
-      options = {
-        userVisibleOnly: true
-      };
-      if (applicationServerKey){
-        options.applicationServerKey = urlB64ToUint8Array(applicationServerKey)
-      }
-      // If not, register one
-      reg.pushManager.subscribe(options)
-        .then(
-          function(subscription) {
-            postSubscribeObj('subscribe', subscription,
-              function(response) {
-                // Check the information is saved successfully into server
-                if (response.status === 201) {
-                  // Show unsubscribe button instead
-                  subBtn.textContent = gettext('Restrict notifications from Waanverse');
-                  subBtn.disabled = false;
-                  isPushEnabled = true;
-                  showMessage(gettext('Notification Permission granted successfully'));
-                }
-              });
-          })
-        .catch(
-          function() {
-            console.log(gettext('Error while granting notification permissions.'), arguments)
-          })
+  reg.pushManager.getSubscription().then(function (subscription) {
+    var metaObj, applicationServerKey, options;
+    // Check if Subscription is available
+    if (subscription) {
+      return subscription;
     }
-  );
+
+    metaObj = document.querySelector('meta[name="django-webpush-vapid-key"]');
+    applicationServerKey = metaObj.content;
+    options = {
+      userVisibleOnly: true,
+    };
+    if (applicationServerKey) {
+      options.applicationServerKey = urlB64ToUint8Array(applicationServerKey);
+    }
+    // If not, register one
+    reg.pushManager
+      .subscribe(options)
+      .then(function (subscription) {
+        postSubscribeObj("subscribe", subscription, function (response) {
+          // Check the information is saved successfully into server
+          if (response.status === 201) {
+            // Show unsubscribe button instead
+            subBtn.textContent = gettext("");
+            subBtn.disabled = true;
+            isPushEnabled = true;
+            showMessage(gettext(""));
+          }
+        });
+      })
+      .catch(function () {
+        console.log(
+          gettext("Error while granting notification permissions."),
+          arguments
+        );
+      });
+  });
 }
 
 function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
 
   const rawData = window.atob(base64);
   const outputArray = new Uint8Array(rawData.length);
@@ -148,44 +142,38 @@ function urlB64ToUint8Array(base64String) {
 
 function unsubscribe(reg) {
   // Get the Subscription to unregister
-  reg.pushManager.getSubscription()
-    .then(
-      function(subscription) {
-
-        // Check we have a subscription to unsubscribe
-        if (!subscription) {
-          // No subscription object, so set the state
-          // to allow the user to subscribe to push
-          subBtn.disabled = false;
-          showMessage(gettext('Subscription is not available.'));
-          return;
-        }
-        postSubscribeObj('unsubscribe', subscription,
-          function(response) {
-            // Check if the information is deleted from server
-            if (response.status === 202) {
-              // Get the Subscription
-              // Remove the subscription
-              subscription.unsubscribe()
-                .then(
-                  function(successful) {
-                    subBtn.textContent = gettext('Allow Notifications');
-                    showMessage(gettext('Notifications restricted.'));
-                    isPushEnabled = false;
-                    subBtn.disabled = false;
-                  }
-                )
-                .catch(
-                  function(error) {
-                    subBtn.textContent = gettext('Restrict notifications from Waanverse');
-                    showMessage(gettext('Error while granting permissions'));
-                    subBtn.disabled = false;
-                  }
-                );
-            }
+  reg.pushManager.getSubscription().then(function (subscription) {
+    // Check we have a subscription to unsubscribe
+    if (!subscription) {
+      // No subscription object, so set the state
+      // to allow the user to subscribe to push
+      subBtn.disabled = false;
+      showMessage(gettext("Subscription is not available."));
+      return;
+    }
+    postSubscribeObj("unsubscribe", subscription, function (response) {
+      // Check if the information is deleted from server
+      if (response.status === 202) {
+        // Get the Subscription
+        // Remove the subscription
+        subscription
+          .unsubscribe()
+          .then(function (successful) {
+            subBtn.textContent = gettext("Allow Notifications");
+            showMessage(gettext("Notifications restricted."));
+            isPushEnabled = false;
+            subBtn.disabled = false;
+          })
+          .catch(function (error) {
+            subBtn.textContent = gettext(
+              "Restrict notifications from Waanverse"
+            );
+            showMessage(gettext("Error while granting permissions"));
+            subBtn.disabled = false;
           });
       }
-    )
+    });
+  });
 }
 
 function postSubscribeObj(statusType, subscription, callback) {
@@ -194,19 +182,22 @@ function postSubscribeObj(statusType, subscription, callback) {
   // and the push subscription endpoint + key the server needs
   // to send push messages
 
-  var browser = navigator.userAgent.match(/(firefox|msie|chrome|safari|trident)/ig)[0].toLowerCase(),
+  var browser = navigator.userAgent
+      .match(/(firefox|msie|chrome|safari|trident)/gi)[0]
+      .toLowerCase(),
     user_agent = navigator.userAgent,
-    data = {  status_type: statusType,
-              subscription: subscription.toJSON(),
-              browser: browser,
-              user_agent: user_agent,
-              group: subBtn.dataset.group
-           };
+    data = {
+      status_type: statusType,
+      subscription: subscription.toJSON(),
+      browser: browser,
+      user_agent: user_agent,
+      group: subBtn.dataset.group,
+    };
 
   fetch(subBtn.dataset.url, {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
+    method: "post",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-    credentials: 'include'
+    credentials: "include",
   }).then(callback);
 }
